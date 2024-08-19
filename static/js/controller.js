8361a9eb9ae6ff955fdcb817eb95b2c95f8bfeb0
able c j <gracyebin7@gamil.com>
2024-08-19 14:22:54 +0530

    some minor changes added like toggler and some css properties

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,5 +1,37 @@
 
 // toggle navbar function defintion
+// toggle navbar function defintion
+function toggleNavbarWidth() {
+  const navbar = document.querySelector('.navbar_desk');
+  const imgButtonSmall = document.querySelector('.toogle_btn_smallsize');
+  const allDivs = navbar.querySelectorAll('div');
+  const navbarSectionInner = navbar.querySelector('.navbar_section_inner');
+  const dashboardNavTitle = navbar.querySelector('.dashbord_nav_title');
+  const logoContainer = navbar.querySelector('.logo_Container')
+
+  if (navbar.style.width === '5%') {
+    // Expand the navbar
+    navbar.style.width = 'auto';
+    imgButtonSmall.style.display = 'none';
+    allDivs.forEach(div => {
+      div.style.display = 'flex';
+      if (div === navbarSectionInner) {
+        div.style.flexDirection = 'column';
+      }
+    });
+    dashboardNavTitle.style.display = 'block';
+  } else {
+    // Collapse the navbar
+    navbar.style.width = '5%';
+    imgButtonSmall.style.display = 'block';
+    allDivs.forEach(div => {
+      div.style.display = 'none';
+    });
+    navbarSectionInner.style.flexDirection = 'column'; // Ensure it is set to column on collapse
+    dashboardNavTitle.style.display = 'none';
+    logoContainer.style.display = 'none';
+  }
+}
 
 
 
